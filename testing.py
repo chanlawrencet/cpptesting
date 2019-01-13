153b5e734cb6e2d4b4c7f9d21d937b2b502c1b94
lchan05 <lchan05@eecs.tufts.edu>
2019-01-13 00:11:33 -0500

    adding main testing

--- testing.py
+++ testing.py
@@ -4,55 +4,116 @@
 # Goal: use this to write bash tests
 
 import subprocess
-
+import os
 
 def main():
-	hwName = input('hwName:')
-	numTests = countFiles()
-	print("numTests:", numTests);
-	executable = input('executable:')
-	dirsToMake = ['out','ref','vout']
-	inpath = [];
-	refpath = [];
-	outpath = [];
 
-	## set up directories
-	for dir in dirsToMake:
-		reMkdir(dir)
+    hwName = input('hwName:')
+    useMains = True if input('main.cpp testing (m) or ref testing (r)?') == 'm' else False
+    checkDirs(useMains) # make sure dirs exist
+    numTests = countTests(useMains) # count num tests
+    checkFiles(numTests) # make sure there are tests
+    checkMakefile(numTests) # if using mains, check if Makefile exists
+    print("numtests:", numTests)
+    executable = None
+    if (useMains== False):
+        executable = input('executable:') # if using ref, executable is the same
 
-	## setup lists
-	for x in range(1, numTests + 1):
-		testName = 'test' + str(x)
-		inpath.append('in/' + testName + '.in')
-		refpath.append('ref/' + testName + '.ref')
-		outpath.append('out/' + testName + '.out')
-		outpath.append('vout/' + testName + '.vout')
+    dirsToMake = ['out','ref','vout', 'tmp']
+    testnames = []
+    inpath = []
+    mainpath = []
+    refpath = []
+    outpath = []
+    voutpath = []
 
-	# generate reference output
-	for x in range(0, numTests):
-		subprocess.call("./" + executable + "  < " + 
-			            inpath[x] +  " > " + refpath[x], shell=True)
+    ## set up directories
+    for dir in dirsToMake:
+        reMkdir(dir)
 
-# counts # of files inside in/, up to 99
-def countFiles():
-	out = subprocess.Popen("ls in| wc -l", stdout=subprocess.PIPE, stderr=subprocess.STDOUT , shell=True)
-	stdout = out.communicate()[0]
-	splitted = list(str(stdout))
-	if (len(splitted) == 6):
-		return int(splitted[2])
-	elif (len(splitted) == 7):
-		return int(splitted[2]) * 10 + splitted[3]
-	else:
-		print("too many tests")
-		exit(1)
+    ## setup lists
+    for x in range(1, numTests + 1):
+        testName = 'test' + addZero(x)
+        testnames.append(str(testName))
+        inpath.append('in/' + testName + '.in')
+        mainpath.append('main/' + testName + '.main')
+        refpath.append('ref/' + testName + '.ref')
+        outpath.append('out/' + testName + '.out')
+        voutpath.append('vout/' + testName + '.vout')
+
+    print(inpath, mainpath, refpath, outpath, voutpath)
+
+
+    ## generate reference output
+    if useMains == True:
+        for x in range(0, numTests):
+            print("cp " + mainpath[x] + " tmp")
+            print("cp bin/Makefile .")
+            print("make "+ testnames[x] + " -C tmp")
+            print("rm temp")
+        exit(0)
+        subprocess.call("make -C bin/", shell=True)
+        subprocess.call("ls", shell=True)
+    else:
+        for x in range(0, numTests):
+            subprocess.call("./" + executable + "  < " + 
+                            inpath[x] +  " > " + refpath[x], shell=True)
+# convert nonzero int into string with leading zero
+def addZero(inputNum):
+    if inputNum < 10:
+        return "0" + str(inputNum)
+    else:
+        return str(inputNum)
+# check if /bin/Makefile exists
+def checkMakefile(useMains):
+    if useMains == False:
+        return
+    if not os.path.isfile('./bin/Makefile'):
+        print('./bin/Makefile not found! Aborting.')
+        exit(1)
+# check if dirs exist
+def checkDirs(useMains):
+    if (useMains == True) and (not os.path.isdir('./main')):
+        print('./main not found! Aborting.')
+        exit(1)
+    elif (useMains == False) and (not os.path.isdir('./in')):
+        print('./in not found! Aborting.')
+        exit(1)
+
+# make sure # tests is non 0
+def checkFiles(numTests):
+    if (numTests == 0):
+        print('no tests found! Aborting.')
+        exit(1)
+
+# counts # of files inside dir/, up to 99
+def countTests(useMains):
+    out = None
+    if (useMains == True):
+        # counts # files in main/ (for main unit testing)
+        out = subprocess.Popen("ls main| wc -l", stdout=subprocess.PIPE, stderr=subprocess.STDOUT , shell=True)
+    else:
+        # counts # files in in/ (for ref comparision)
+        out = subprocess.Popen("ls in| wc -l", stdout=subprocess.PIPE, stderr=subprocess.STDOUT , shell=True)
+    stdout = out.communicate()[0]
+    splitted = list(str(stdout))
+
+    # turn list back into number, max 99
+    if (len(splitted) == 6):
+        return int(splitted[2])
+    elif (len(splitted) == 7):
+        return int(splitted[2]) * 10 + int(splitted[3])
+    else:
+        print("Too many tests! Aborting.") # max = 99
+        exit(1)
 
 def echo(input):
-	print('echo \"' + input + '\"')
+    print('echo \"' + input + '\"')
 
 # deletes and remakes directories
 def reMkdir(input):
-	subprocess.call("rm -rf " + input, shell=True)
-	subprocess.call("mkdir " + input, shell=True)
+    subprocess.call("rm -rf " + input, shell=True)
+    subprocess.call("mkdir " + input, shell=True)
 
 
 if __name__ == "__main__":
